816b094abcc86c7f65ff229b14ef17c909bb01d3
Vladimir <walldemarsokol@yandex.ru>
2023-05-09 13:18:36 +0300

    correct HW

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,8 @@
 // Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 
-// Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+// Дан следующий код, исправьте его там,
+// где требуется
+// (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 public class Test2 {
     public static void main(String[] args) {
         foo1();
